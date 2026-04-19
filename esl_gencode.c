d210e753d620128e523d369e724b65ca65dac146
Genevieve Krause <genevievekrause@gmail.com>
2026-04-19 12:48:59 -0700

    Prevent wasteful string operations ORF name and desctiption formating by esl_gencode_ProcessOrf are needed for esl-translate but not for bathsearch.

--- esl_gencode.c
+++ esl_gencode.c
@@ -791,8 +791,10 @@ esl_gencode_ProcessOrf(ESL_GENCODE_WORKSTATE *wrk, ESL_SQ *sq)
         esl_sq_Grow(psq, /*opt_nsafe=*/NULL);
       psq->dsq[1+psq->n] = eslDSQ_SENTINEL;
 
-      esl_sq_FormatName(psq, "orf%d", wrk->orfcount);
-      esl_sq_FormatDesc(psq, "source=%s coords=%" PRId64 "..%" PRId64 " length=%" PRId64 " frame=%d desc=%s", psq->source, psq->start, psq->end, psq->n, wrk->frame + 1 + (wrk->is_revcomp ? 3 : 0), sq->desc);
+      if (wrk->orf_block == NULL) {  /* esl-translate: name and description go to FASTA output */
+        esl_sq_FormatName(psq, "orf%d", wrk->orfcount);
+        esl_sq_FormatDesc(psq, "source=%s coords=%" PRId64 "..%" PRId64 " length=%" PRId64 " frame=%d desc=%s", psq->source, psq->start, psq->end, psq->n, wrk->frame + 1 + (wrk->is_revcomp ? 3 : 0), sq->desc);
+      }
       /* if we do not have a block to write ORFs to then write ORFs to file */
       if (wrk->orf_block == NULL)
       {
